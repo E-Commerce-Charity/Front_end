7a2039a4117a42e444682e6975a2f4cb39207fca
Hussein-tarek <htarek47@gmail.com>
2023-02-20 23:50:30 +0200

    contact us page

--- src/app/components/contact/contact-form/contact-form.component.ts
+++ src/app/components/contact/contact-form/contact-form.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { faPhone, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent {
-  phone = faPhone;
-  envelope = faEnvelope;
-  location = faLocationDot;
+
 }
