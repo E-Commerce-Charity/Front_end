9542a86cbd0885f637ffab7af76f9698610a5690
Mennat Allah <manoshkamal316@gmail.com>
2023-03-03 16:23:49 +0200

    New Updates

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,35 +1,54 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class userService {
+  // auth_token =
+  //   // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2M2ZmM2JkNTgxNDYxODhjYTNkZjUyNTMiLCJpYXQiOjE2Nzc3OTYzMDB9.06lkv7YCaAqemCuBxqUDiMotGe9sJM4HK5Suy_sPthQ';
+  //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2M2U5NGQ0OWIyNDk3MjdhNDcxMTNmZTMiLCJpYXQiOjE2Nzc4MDU1MTJ9.KU00UhEQCNmpLoIzGiGH-pR1E-d3CwBKd6980V5IfVQ';
+  auth_token: any = localStorage.getItem('token');
+  // id: any = localStorage.getItem('id');
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${this.auth_token}`,
+  });
+
+  params = new HttpParams().set('page', '1').set('limit', '30');
+  options = { params: this.params, headers: this.headers };
+
   users: any;
   tokken: any = localStorage.getItem('token');
   private baseUrl = 'http://localhost:3000/users/';
 
   constructor(private http: HttpClient) {}
   getAllUsers() {
-    return this.http.get(this.baseUrl);
+    return this.http.get(this.baseUrl, {
+      headers: this.headers,
+    });
   }
 
   createUser(user: any) {
     return this.http.post<any>(`http://localhost:3000/auth/register`, user);
   }
 
-  getUserById(id: number) {
+  getUserById(id: any) {
     return this.http.get(`${this.baseUrl}${id}`);
   }
 
-  updateUser(model: any, id: any, editUser: any) {
-    return this.http.put(`${this.baseUrl}${id}`, editUser);
+  updateUser(id: any, editUser: any) {
+    return this.http.patch(`${this.baseUrl}${id}`, editUser, {
+      headers: this.headers,
+    });
   }
 
   deleteUser(id: number) {
     return this.http.delete(`${this.baseUrl}${id}`);
   }
-
+  GetOrder() {
+    return this.http.get('http://localhost:3000/oreders/', this.options);
+  }
   login(user: any) {
     return this.http.post<any>(`http://localhost:3000/auth/login`, user);
   }
